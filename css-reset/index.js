8a0b4e85be0c9d664696cd1b922b538367b5b9cd
AISLVE <aislve@AISLVEs-MacBook-Air.local>
2023-07-20 02:43:25 +0530

    CSS RESET[Major Update]: Full Code refactor

--- css-reset/index.js
+++ css-reset/index.js
@@ -1,49 +1,23 @@
+const toggleButton = document.getElementById("button");
 const navbar = document.getElementById("navbar");
-const button = document.getElementById("button");
-const button2 = document.getElementById("button2")
-const mainContent = document.getElementById("main");
-const body = document.body;
+const myList = document.getElementById("ul")
+const mainContent = document.getElementById("main")
+/* const navbarContent = document.getElementsByName("li") */
 
-let isCollapsed = false;
-
-button2.addEventListener("click", () => {
-  if(document.body.clientWidth >= 911) {
-    if(isCollapsed === true) {
-      navbar.classList.add("slide-in")
-      navbar.classList.remove("slide-out")
-      navbar.classList.add("nav");
-      navbar.classList.remove("none");
-      mainContent.classList.remove("one-by-three");
-      mainContent.classList.add("two-by-three");
-      isCollapsed = false;
-    } else if(isCollapsed === false) {
-      navbar.classList.remove("slide-in")
-      navbar.classList.add("slide-out")
-      setTimeout(() => {
-        navbar.classList.add("none");
-        mainContent.classList.add("one-by-three");
-        navbar.classList.remove("nav")
-      }, 100)
-      mainContent.classList.remove("two-by-three");
-      isCollapsed = true;
-    }
-  } else if(document.body.clientWidth < 911) {
-    if(isCollapsed === true) {
-      navbar.classList.add("mobile-nav");
-      navbar.classList.remove("slide-out")
-      navbar.classList.remove("nav");
-      navbar.classList.remove("none");
-      mainContent.classList.add("one-by-three");
-      mainContent.classList.remove("two-by-three");
-      isCollapsed = false;
-    } else if(isCollapsed === false) {
-      navbar.classList.remove("mobile-nav")
-      navbar.classList.remove("slide-out")
-      navbar.classList.remove("nav")
-      navbar.classList.add("none");
-      mainContent.classList.add("one-by-three");
-      mainContent.classList.remove("two-by-three");
-      isCollapsed = true;
-    }
+toggleButton.addEventListener("click", () => {
+  if(navbar.getAttribute("data-visible") === "false") {
+    navbar.setAttribute("data-visible", true)
+  } else if(navbar.getAttribute("data-visible") === "true") {
+    navbar.setAttribute("data-visible", false)
   }
 })
+
+mainContent.addEventListener("click", () => {
+  if(navbar.getAttribute("data-visible") === "true") {
+    navbar.setAttribute("data-visible", false)
+  }
+})
+
+myList.addEventListener("click", () => {
+  navbar.setAttribute("data-visible", false)
+});
