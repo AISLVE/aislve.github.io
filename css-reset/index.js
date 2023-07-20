6ca7f3ee179806bf4966926b42aaf6b469680b6e
AISLVE <aislve@AISLVEs-MacBook-Air.local>
2023-07-20 12:50:06 +0530

    CSS RESET[Minor Changes]: Changed some font sizes and imroved navbar's responsiveness

--- css-reset/index.js
+++ css-reset/index.js
@@ -2,22 +2,25 @@ const toggleButton = document.getElementById("button");
 const navbar = document.getElementById("navbar");
 const myList = document.getElementById("ul")
 const mainContent = document.getElementById("main")
-/* const navbarContent = document.getElementsByName("li") */
 
 toggleButton.addEventListener("click", () => {
   if(navbar.getAttribute("data-visible") === "false") {
     navbar.setAttribute("data-visible", true)
+    toggleButton.setAttribute("aria-expanded", true)
   } else if(navbar.getAttribute("data-visible") === "true") {
     navbar.setAttribute("data-visible", false)
+    toggleButton.setAttribute("aria-expanded", false)
   }
 })
 
 mainContent.addEventListener("click", () => {
   if(navbar.getAttribute("data-visible") === "true") {
     navbar.setAttribute("data-visible", false)
+    toggleButton.setAttribute("aria-expanded", false)
   }
 })
 
 myList.addEventListener("click", () => {
   navbar.setAttribute("data-visible", false)
+  toggleButton.setAttribute("aria-expanded", false)
 });
